bd38dae679ffdd40c47d59ec156b9926c1d96b3b
Suzzybee <ateresusan92@gmail.com>
2023-11-30 08:39:49 +0100

    fgjkuwkmsnwkjjjw

--- shopping-list.js
+++ shopping-list.js
@@ -1,16 +1,16 @@
   // Function to add an item to the shopping list
 function addItem() {
-    const name = document.getElementById('item-name').value;
-    const quantity = parseInt(document.getElementById('item-quantity').value);
-    const price = parseFloat(document.getElementById('item-price').value);
-
+    let name = document.getElementById('item-name').value;
+    let quantity = Number(document.getElementById('item-quantity').value);
+    let price = Number(document.getElementById('item-price').value);
+ 
     if (!name || isNaN(quantity) || isNaN(price)) {
         alert('Please enter valid item details.');
         return;
     }
 
-    const item = { name, quantity, price };
-    const shoppingList = JSON.parse(localStorage.getItem('shoppingList')) || [];
+    let item = { name, quantity, price };
+    let shoppingList = JSON.parse(localStorage.getItem('shoppingList')) || [];
     shoppingList.push(item);
     localStorage.setItem('shoppingList', JSON.stringify(shoppingList));
 
@@ -19,35 +19,40 @@ function addItem() {
 
 // Function to update the shopping receipt
 function updateReceipt() {
-    const shoppingList = JSON.parse(localStorage.getItem('shoppingList')) || [];
-    const totalElement = document.getElementById('total');
-    const shoppingListElement = document.getElementById('shopping-list');
+    let shoppingList = JSON.parse(localStorage.getItem('shoppingList')) || [];
+    let totalElement = document.getElementById('total');
+    let shoppingListElement = document.getElementById('shopping-list');
 
     let total = 0;
 
     shoppingListElement.innerHTML = '';
     shoppingList.forEach((item, index) => {
-        const listItem = document.createElement('li');
-        const itemTotal = item.quantity * item.price;
-      listItem.innerHTML = `<strong>Item:</strong> ${item.name} | <strong class="ms-4 ">Quantity:</strong> ${item.quantity} | <strong class="ms-4 ">Price:</strong> ₦${itemTotal.toFixed(2)} 
+        let listItem = document.createElement('li');
+        let itemTotal = item.quantity * item.price;
+      listItem.innerHTML = 
+
+      `<strong>Item:</strong> ${item.name}  <strong class="ms-4 ">Quantity:</strong> ${item.quantity}  <strong class="ms-4 ">Price:</strong> ₦${itemTotal.toFixed(2)} 
           <button onclick="editItem(${index})" class="btn btn-dark ms-5 me-5 mb-2">Edit</button> 
-          <button onclick="deleteItem(${index})" class="btn btn-danger">Delete</button>`;
+          <button onclick="deleteItem(${index})" class="btn btn-danger">Delete</button><input onclick="addItemList()" style="        width: 13px; box-shadow:none; " type="checkbox" class="ms-4">
+          `;
         shoppingListElement.appendChild(listItem);
         total += itemTotal;
     });
 
     totalElement.textContent = total.toFixed(2);
 }
-
+function addItemList(){
+    
+}
 // Function to edit an item
 function editItem(index) {
-    const shoppingList = JSON.parse(localStorage.getItem('shoppingList')) || [];
-    const item = shoppingList[index];
+    let shoppingList = JSON.parse(localStorage.getItem('shoppingList')) || [];
+    let item = shoppingList[index];
     if (!item) return;
 
-    const newName = prompt('Edit item name:', item.name);
-    const newQuantity = parseInt(prompt('Edit item quantity:', item.quantity));
-    const newPrice = parseFloat(prompt('Edit item price:', item.price));
+    let newName = prompt('Edit item name:', item.name);
+    let newQuantity = Number(prompt('Edit item quantity:', item.quantity));
+    let newPrice = Number(prompt('Edit item price:', item.price));
 
     if (!newName || isNaN(newQuantity) || isNaN(newPrice)) {
         alert('Invalid input. Item not updated.');
@@ -65,7 +70,7 @@ function editItem(index) {
 
 // Function to delete an item
 function deleteItem(index) {
-    const shoppingList = JSON.parse(localStorage.getItem('shoppingList')) || [];
+    let shoppingList = JSON.parse(localStorage.getItem('shoppingList')) || [];
     shoppingList.splice(index, 1);
     localStorage.setItem('shoppingList', JSON.stringify(shoppingList));
     updateReceipt();
